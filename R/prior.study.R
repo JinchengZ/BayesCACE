50a625fc70647886408bfadd04cbb462e9d50f15
Jincheng Zhou <jzhou@ad.umn.edu>
2019-06-24 19:25:00 -0500

    revise

---
+++ R/prior.study.R
@@ -0,0 +1,31 @@
+<<<<<<< HEAD
+prior.single <- function(prior.type="custom"){
+  
+  string2 <-   
+  "# priors
+  n ~ dnorm(0, 0.01)
+  a ~ dnorm(0, 0.01)
+  alpha.s ~ dnorm(0, 0.01)
+  alpha.b ~ dnorm(0, 0.01)
+  alpha.u ~ dnorm(0, 0.01)
+  alpha.v ~ dnorm(0, 0.01)
+  }"
+
+return(string2)
+}
+=======
+prior.single <- function(prior.type="custom"){
+  
+  string2 <-   
+  "# priors
+  n ~ dnorm(0, 0.01)
+  a ~ dnorm(0, 0.01)
+  alpha.s ~ dnorm(0, 0.01)
+  alpha.b ~ dnorm(0, 0.01)
+  alpha.u ~ dnorm(0, 0.01)
+  alpha.v ~ dnorm(0, 0.01)
+  }"
+
+return(string2)
+}
+>>>>>>> 3c97488a87eeaca4b1b2ab7f216b9673bdb46721
